670b9da3296aa946bab1a45513a359c674cd456b
MackBook <dimashari215@gmail.com>
2024-10-07 20:21:13 +0700

    Tugas Baru

--- Pekerjaan11.java
+++ Pekerjaan11.java
@@ -2,28 +2,24 @@ import java.util.Scanner;
 
 public class Pekerjaan11 {
 
+    
     public static void main(String[] args) {
-        // Membuat scanner untuk input dari pengguna
         Scanner scanner = new Scanner(System.in);
 
-        // Meminta pengguna memasukkan angka
-        System.out.print("Masukkan angka antara 1 hingga 10: ");
+        System.out.print("Masukkan angka antara 1 hingga 100: ");
         int angka = scanner.nextInt();
 
-        // Memeriksa apakah angka berada dalam rentang 1 hingga 10
-        if (angka < 1 || angka > 10) {
+        if (angka < 1 || angka > 100) {
             System.out.println("Angka di luar jangkauan");
         } else {
-            // Memeriksa apakah angka ganjil atau genap
-            if (angka == 1 || angka == 3 || angka == 5 || angka == 7 || angka == 9) {
-                System.out.println("Angka " + angka + " adalah ganjil.");
-            } else if (angka == 2 || angka == 4 || angka == 6 || angka == 8 || angka == 10) {
+
+            if (angka % 2 == 0) {
                 System.out.println("Angka " + angka + " adalah genap.");
+            } else {
+                System.out.println("Angka " + angka + " adalah ganjil.");
             }
         }
-
         // Menutup scanner
         scanner.close();
     }
 }
-
